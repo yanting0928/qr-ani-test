cd19ec3940f7101c632327ce031b9cd491cbf40f
yanting0928 <1303697177@qq.com>
2021-11-11 22:47:14 +0800

    push results for perturbed perfect helix

---
+++ cctbx/lbfgs_b/running_script.sh
@@ -0,0 +1,12 @@
+for i in 0.3  0.6  0.9  1.2  1.5; do
+cd $i
+
+for k in $(seq 0 9);do
+echo " -- $i.$k --"
+
+phenix.python /home/yanting/work/aniserver_1_qr/ani_qr_test/cctbx_lbfgs/qr-ani-test/run.py $k.pdb restraints=cctbx minimizer=lbfgs_b stpmax=0.2 max_itarations=50 macro_cycles=50 > $k.log 2>&1
+
+done
+
+cd ..
+done
