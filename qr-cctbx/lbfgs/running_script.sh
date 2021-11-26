582cd7162a2b2c41058e746b8ed95606e4cfc2c8
yanting0928 <1303697177@qq.com>
2021-11-26 21:29:06 +0800

    git add result with qr.refine

---
+++ qr-cctbx/lbfgs/running_script.sh
@@ -0,0 +1,17 @@
+for i in 0.3  0.6  0.9  1.2  1.5; do
+cd $i
+
+for k in $(seq 0 9);do
+echo " -- $i.$k --"
+
+qr.refine $k.pdb  mode=opt gradient_only=true restraints=cctbx clustering=false number_of_micro_cycles=100 max_iterations_refine=50 minimizer=lbfgs > $k.log 2>&1
+
+cp  pdb/*_refined.pdb .
+
+fold_name=$k"_pdb"
+mv pdb/ $fold_name/
+
+done
+
+cd ..
+done
