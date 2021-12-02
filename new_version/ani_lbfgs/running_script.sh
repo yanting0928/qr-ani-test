6926cb99d94459fcad789e72bbdda6aab49aea1b
yanting0928 <1303697177@qq.com>
2021-12-02 11:48:12 +0800

    add test result for new script and qrefine

---
+++ new_version/ani_lbfgs/running_script.sh
@@ -0,0 +1,14 @@
+for i in 0.3  0.6  0.9  1.2  1.5; do
+cd $i
+
+for k in $(seq 0 9);do
+echo " -- $i.$k --"
+
+prefix=$k"_minimized"
+
+phenix.python /home/yanting/work/aniserver_1_qr/ani_qr_test/cctbx_lbfgs/qr-ani-test/run.py $k.pdb restraints=ani minimizer=lbfgs stpmax=0.2 max_itarations=2500 prefix=$prefix gradient_only=true > $k.log 2>&1
+
+done
+
+cd ..
+done
---
+++ new_version/qr_ani_lbfgs/running_script.sh
@@ -0,0 +1,17 @@
+for i in 0.3  0.6  0.9  1.2  1.5; do
+cd $i
+
+for k in $(seq 0 9);do
+echo " -- $i.$k --"
+
+qr.refine $k.pdb  mode=opt  restraints=qm engine_name=torchani clustering=false stpmax=0.2 max_iterations_refine=2500 minimizer=lbfgs number_of_micro_cycles=1 gradient_only=true > $k.log 2>&1
+
+cp  pdb/*_refined.pdb .
+
+fold_name=$k"_pdb"
+mv pdb/ $fold_name/
+
+done
+
+cd ..
+done
